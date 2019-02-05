c77d3915bf7ac32503670c32f8f81e00c1304c0a
Matteo Muscas <thatman.j@gmail.com>
2019-02-05 12:15:42 +0000

    sections now work

--- lib/ilpost/cli.rb
+++ lib/ilpost/cli.rb
@@ -3,7 +3,6 @@
 class Ilpost::CLI
 
   def call
-    list_home
     menu
   end
 
@@ -17,12 +16,11 @@ class Ilpost::CLI
       puts "   #{article[:url]}".colorize(:red)
     end
   end
-  
-  def list_sections(url)
-    url = Ilpost::Post.section(url)
-    puts "Ecco i titoli della sezione:".colorize(:green)
-    puts "-------------------------".colorize(:green)
-    articles = Ilpost::Post.section
+
+  def list_section(url)
+    puts "Ecco i titoli della sezione #{url.capitalize}:".colorize(:green)
+    puts "---------------------------------------------".colorize(:green)
+    articles = Ilpost::Post.section(url)
     articles.each.with_index(1) do |article, index|
       puts "#{index}. #{article[:title]}".colorize(:blue)
       puts "   #{article[:subtitle]}"
@@ -33,17 +31,46 @@ class Ilpost::CLI
   def menu
     input = nil
     while input != "esci"
-      puts "Seleziona un numero per aprire l'articolo, o digita: \n
-      per vedere più titoli: avanti \n
-      per uscire: esci \n
-      o digita una delle seguenti sezioni: \n
-      italia - mondo - politica - tecnologia - internet \n
-      scienza - cultura - economia - sport - media \n
-      moda - libri - auto - video \n"
+      puts "Benvenuto!"
+      puts "Digita 'home' per i titoli principali, o digita un numero per aprire un articolo."
+      puts "Digita 'esci' per uscire o digita una delle seguenti sezioni:"
+      puts "italia - mondo - politica - tecnologia - internet - scienza - cultura - economia - sport - media - moda - libri - auto - video \n".colorize(:yellow)
       input = gets.strip.downcase
-      
-      if input == "italia"
-        list_sections(italia)
+
+      if input == "home"
+        self.list_home
+      elsif input == "italia"
+        self.list_section(input)
+      elsif input == "mondo"
+        self.list_section(input)
+      elsif input == "politica"
+        self.list_section(input)
+      elsif input == "tecnologia"
+        self.list_section(input)
+      elsif input == "internet"
+        self.list_section(input)
+      elsif input == "scienza"
+        self.list_section(input)
+      elsif input == "cultura"
+        self.list_section(input)
+      elsif input == "economia"
+        self.list_section(input)
+      elsif input == "sport"
+        self.list_section(input)
+      elsif input == "media"
+        self.list_section(input)
+      elsif input == "moda"
+        self.list_section(input)
+      elsif input == "libri"
+        self.list_section(input)
+      elsif input == "auto"
+        self.list_section(input)
+      elsif input == "video"
+        self.list_section(input)
+      elsif input =="esci"
+        break
+      else
+        puts "Mi spiace, non ho capito.".colorize(:red)
       end
     end
   end
